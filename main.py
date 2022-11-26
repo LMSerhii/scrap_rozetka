260caa36909042217e29ad9450d5e017e5166a74
Serhii Leonov <leonovserhii89@gmail.com>
2022-11-26 07:53:30 +0200

    вынес куки и заголовки в отдельный файл, добавил запись в json и csv, почистил лишнее

--- main.py
+++ main.py
@@ -1,75 +1,30 @@
+import csv
 import json
 
 import requests
 
+from headers_and_cookies import headers, cookies
+
 
 def get_data():
     """ """
-    import requests
-
-    cookies = {
-        'ab_language': 'new',
-        'slang': 'ua',
-        'uid': 'Cgo8kGNZb9o1OnVN5+DbAg==',
-        'delivery': 'new',
-        'uss_evoid_cascade': 'yes',
-        'ab-catalog-backend': 'old',
-        'cart-modal': 'cart-page',
-        'ab-auto-portal': 'old',
-        'filter-tabs': 'old',
-        'ab-catalog-delivery-terms': 'old',
-        'ab-catalog-selection-filters': 'old',
-        'promo-horizontal-filters': 'splitFilters',
-        'ab-catalog-tile-description': 'old',
-        'ab-catalog-filter-result-quantity': 'old',
-        'ab-kt-action': 'super-offer',
-        'fingerprint': 'off',
-        'skip-add-phone': 'off',
-        'xab_segment': '148',
-        '_uss-csrf': 'qOgS7G6XmbsRWyta4Yf6uHt+b0pE8URxjxvSc5ii57y8YFYQ',
-        'ussapp': '-jnO_-JA_3j_4auwe_ddB11RnXcVXsOC_eFtZ6nz',
-        'visitor_city': '1',
-        '_gcl_au': '1.1.82716467.1666805727',
-        '_ga': 'GA1.3.1649475729.1666805727',
-        'afclid': '7558957401666805727',
-        '_fbp': 'fb.2.1666805727628.99665783',
-        '__exponea_etc__': 'bc3d4da0-3fd5-4476-a2ce-3a3e2f43486a',
-        'social-auth': 'old',
-        'fit-size': 'new',
-        'ab-catalog-filters-index': 'off',
-        'af_source': 'adwords',
-        'af_sources': '{"adwords":["1669196740","0"]}',
-        'ussat_exp': '1669239941',
-        'ussajwt': 'eyJhbGciOiJSUzI1NiIsImtpZCI6InVzc2F0LnYwIiwidHlwIjoiSldUIn0.eyJkZXRhaWxzIjoiYzk1OGM3YzQwZWFhMTlkNjgyMTk2MGIyY2YyOTk2MTg3ZjFhN2U4Yjc3Y2RhYTY4M2JkNThiYjBlZmZlYmY1NzA3MTgwNTFmYTE4MTc0MzU1MThiZDFhYjM5NmJhZGY1NzVkYTMzYjYyMmM0ZTk2NWQ1ZThmODUxMWUzZDliZTYzYmY2NjY0ZGIwN2EyM2JjNWFkMDU2ZDU4OGZjNGM0YjllNjZkZTc0ZWI3YWEwZDc0YTBhNWFiMDAzMGM2ZTQ0MmI4YzMzMjRiNjc4Y2YwZjg5MjE5ZjgyMDg0OWJhNmM1OTg2NjY5MDIzNTFiYjFiYTg0N2ZlYjQwMmVkNWQxMSIsImV4cCI6IjE2NjkyMzk5NDEiLCJ1c3NhdCI6ImFhMWUxYmM1NDljZDQ2ZDUzMzZhZTY4MDU1MjE3Zjk3LnVhLWU2OGRiOTIzZDQxYWViMGRlMjBmODJlY2E3MDM2YTRiLjE2NjkyMzk5NDEifQ.S7Xc0BtKK8OZSUEZmt_4D93EorFVknkkvFNWfx0X8_eZMFpNFNBFXvup5RIIr2tvcxfqPDVsDPWdvmgPo389VKKwk18WV2_9WmY-sLnueibhXhtUiqf9chtGIWEa7Ho_fKerBFbChlhrCc6q30UMB5eDUZ163Cnptk4T7fK-h7kUiAwaNlED7dOGQZuBg6JMWgM5fnBZFEdZmmJF2Qtn8r7rSmYGzU8yuzfOJ0Yx_-UUUroaWLxpQ0GNiLXy9HLWqvAZdxboXt2g1lATdDicTkpXYrxAURysDN6zRwf3fM1PqDxttz2MbTIBTyj6fL0ZesjPg2TAelvaKUkZkhthbA',
-        'ussat': 'aa1e1bc549cd46d5336ae68055217f97.ua-e68db923d41aeb0de20f82eca7036a4b.1669239941',
-        'ussrt': '38d6a33c0c8777c7d0f860a9b828f539.ua-e68db923d41aeb0de20f82eca7036a4b.1671788741',
-        '_gcl_aw': 'GCL.1669189545.CjwKCAiApvebBhAvEiwAe7mHSJzugRq0z8TuD5_gLbIAa2I43ZF7S8um0eNjY4bOUJsas66_6_GhERoCPSsQAvD_BwE',
-        '__utmz_gtm': 'utmcsr=google|utmccn=adwords|utmcmd=cpc',
-        '_gid': 'GA1.3.2120376417.1669189545',
-        '_gac_UA-203518-6': '1.1669189545.CjwKCAiApvebBhAvEiwAe7mHSJzugRq0z8TuD5_gLbIAa2I43ZF7S8um0eNjY4bOUJsas66_6_GhERoCPSsQAvD_BwE',
-        '__exponea_time2__': '7200.225232839584',
-        'utm_source': 'adwords',
-        '_dc_gtm_UA-203518-6': '1',
-        '__cf_bm': 'kKTnbO7I810oHXBrnyeYA5yA0QqgG6TY9ptu9EPhtEQ-1669198315-0-AVceCDMaiGbNK3zKCLpd4jPRymykqHGskJ8NmGfvrv7S1Nr31U6uWu5Aw5NC0dV6jKsKpf73zuv+7JV0FovJ6C0=',
-    }
-
-    headers = {
-        'authority': 'search.rozetka.com.ua',
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,zh;q=0.5',
-        'content-type': 'application/x-www-form-urlencoded; charset=utf-8',
-        # Requests sorts cookies= alphabetically
-        # 'cookie': 'ab_language=new; slang=ua; uid=Cgo8kGNZb9o1OnVN5+DbAg==; delivery=new; uss_evoid_cascade=yes; ab-catalog-backend=old; cart-modal=cart-page; ab-auto-portal=old; filter-tabs=old; ab-catalog-delivery-terms=old; ab-catalog-selection-filters=old; promo-horizontal-filters=splitFilters; ab-catalog-tile-description=old; ab-catalog-filter-result-quantity=old; ab-kt-action=super-offer; fingerprint=off; skip-add-phone=off; xab_segment=148; _uss-csrf=qOgS7G6XmbsRWyta4Yf6uHt+b0pE8URxjxvSc5ii57y8YFYQ; ussapp=-jnO_-JA_3j_4auwe_ddB11RnXcVXsOC_eFtZ6nz; visitor_city=1; _gcl_au=1.1.82716467.1666805727; _ga=GA1.3.1649475729.1666805727; afclid=7558957401666805727; _fbp=fb.2.1666805727628.99665783; __exponea_etc__=bc3d4da0-3fd5-4476-a2ce-3a3e2f43486a; social-auth=old; fit-size=new; ab-catalog-filters-index=off; af_source=adwords; af_sources={"adwords":["1669196740","0"]}; ussat_exp=1669239941; ussajwt=eyJhbGciOiJSUzI1NiIsImtpZCI6InVzc2F0LnYwIiwidHlwIjoiSldUIn0.eyJkZXRhaWxzIjoiYzk1OGM3YzQwZWFhMTlkNjgyMTk2MGIyY2YyOTk2MTg3ZjFhN2U4Yjc3Y2RhYTY4M2JkNThiYjBlZmZlYmY1NzA3MTgwNTFmYTE4MTc0MzU1MThiZDFhYjM5NmJhZGY1NzVkYTMzYjYyMmM0ZTk2NWQ1ZThmODUxMWUzZDliZTYzYmY2NjY0ZGIwN2EyM2JjNWFkMDU2ZDU4OGZjNGM0YjllNjZkZTc0ZWI3YWEwZDc0YTBhNWFiMDAzMGM2ZTQ0MmI4YzMzMjRiNjc4Y2YwZjg5MjE5ZjgyMDg0OWJhNmM1OTg2NjY5MDIzNTFiYjFiYTg0N2ZlYjQwMmVkNWQxMSIsImV4cCI6IjE2NjkyMzk5NDEiLCJ1c3NhdCI6ImFhMWUxYmM1NDljZDQ2ZDUzMzZhZTY4MDU1MjE3Zjk3LnVhLWU2OGRiOTIzZDQxYWViMGRlMjBmODJlY2E3MDM2YTRiLjE2NjkyMzk5NDEifQ.S7Xc0BtKK8OZSUEZmt_4D93EorFVknkkvFNWfx0X8_eZMFpNFNBFXvup5RIIr2tvcxfqPDVsDPWdvmgPo389VKKwk18WV2_9WmY-sLnueibhXhtUiqf9chtGIWEa7Ho_fKerBFbChlhrCc6q30UMB5eDUZ163Cnptk4T7fK-h7kUiAwaNlED7dOGQZuBg6JMWgM5fnBZFEdZmmJF2Qtn8r7rSmYGzU8yuzfOJ0Yx_-UUUroaWLxpQ0GNiLXy9HLWqvAZdxboXt2g1lATdDicTkpXYrxAURysDN6zRwf3fM1PqDxttz2MbTIBTyj6fL0ZesjPg2TAelvaKUkZkhthbA; ussat=aa1e1bc549cd46d5336ae68055217f97.ua-e68db923d41aeb0de20f82eca7036a4b.1669239941; ussrt=38d6a33c0c8777c7d0f860a9b828f539.ua-e68db923d41aeb0de20f82eca7036a4b.1671788741; _gcl_aw=GCL.1669189545.CjwKCAiApvebBhAvEiwAe7mHSJzugRq0z8TuD5_gLbIAa2I43ZF7S8um0eNjY4bOUJsas66_6_GhERoCPSsQAvD_BwE; __utmz_gtm=utmcsr=google|utmccn=adwords|utmcmd=cpc; _gid=GA1.3.2120376417.1669189545; _gac_UA-203518-6=1.1669189545.CjwKCAiApvebBhAvEiwAe7mHSJzugRq0z8TuD5_gLbIAa2I43ZF7S8um0eNjY4bOUJsas66_6_GhERoCPSsQAvD_BwE; __exponea_time2__=7200.225232839584; utm_source=adwords; _dc_gtm_UA-203518-6=1; __cf_bm=kKTnbO7I810oHXBrnyeYA5yA0QqgG6TY9ptu9EPhtEQ-1669198315-0-AVceCDMaiGbNK3zKCLpd4jPRymykqHGskJ8NmGfvrv7S1Nr31U6uWu5Aw5NC0dV6jKsKpf73zuv+7JV0FovJ6C0=',
-        'origin': 'https://rozetka.com.ua',
-        'referer': 'https://rozetka.com.ua/ua/seller/instrumentfree/goods/',
-        'sec-ch-ua': '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-    }
+    with open("data/data.csv", "a", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(
+            (
+                "Бренд",
+                "Категорія",
+                "Назва",
+                "id",
+                "Прайс USD",
+                "Прайс UAH",
+                "Прайс зі знижкою ",
+                "Опис",
+                "Посилання на карточку товару",
+                "Посилання на зображення",
+                "Посилання на відео",
+            )
+        )
 
     params = {
         'front-type': 'xl',
@@ -84,9 +39,13 @@ def get_data():
                             headers=headers).json()
 
     total_products = response['data']['quantities']['goods_quantity_total_found']
-    total_pages = response['data']['pagination']['total_pages']
-    page_size = response['data']['pagination']['page_size']
     category_name = [category['option_value_name'] for category in response['data']['options'][0]['option_values']]
+
+    data = {}
+    products = []
+    count_items = 0
+    count_images = 0
+    count_videos = 0
 
     for category in category_name[:1]:
 
@@ -102,8 +61,80 @@ def get_data():
         response = requests.get('https://search.rozetka.com.ua/ua/seller/api/v6/', params=params, cookies=cookies,
                                 headers=headers).json()
 
-        with open("procraft.json", "w", encoding="utf-8") as file:
-            json.dump(response, file, indent=4, ensure_ascii=False)
+        goods = response['data']['goods']
+        for good in goods:
+            id = good['id']
+
+            params = {
+                'front-type': 'xl',
+                'country': 'UA',
+                'lang': 'ua',
+                'goodsId': id,
+            }
+
+            response = requests.get('https://rozetka.com.ua/api/product-api/v4/goods/get-main', params=params,
+                                    cookies=cookies,
+                                    headers=headers).json()
+
+            id = response.get("data").get("id")
+            title = response.get("data").get("title")
+            price = response.get("data").get("price")
+            old_price = response.get("data").get("old_price")
+            price_pcs = response.get("data").get("price_pcs")
+            href = response.get("data").get("href")
+            brand = response.get("data").get("brand")
+            last_category = response.get("data").get("last_category").get("title")
+            images = [image.get("original").get("url") for image in response.get("data").get("images")]
+            videos = response.get("data").get("videos")
+            description = response.get("data").get("description").get("text")
+
+            products.append(
+                {
+                    "Бренд": brand,
+                    "Категорія": last_category,
+                    "Назва": title,
+                    "id": id,
+                    "Прайс USD": price_pcs,
+                    "Прайс UAH": old_price,
+                    "Прайс зі знижкою ": price,
+                    "Опис": description,
+                    "Посилання на карточку товару": href,
+                    "Посилання на зображення": images,
+                    "Посилання на відео": videos,
+                }
+            )
+
+            with open("data/data.csv", "a", encoding="utf-8") as file:
+                writer = csv.writer(file)
+                writer.writerow(
+                    (
+                        brand,
+                        last_category,
+                        title,
+                        id,
+                        price_pcs,
+                        old_price,
+                        price,
+                        description,
+                        href,
+                        images,
+                        videos
+                    )
+                )
+            count_items += 1
+            count_images += 1
+            count_videos += 1
+
+            print(f"{count_items}/{total_products}")
+
+    data["products"] = products
+    data["count_items"] = count_items
+    data["count_images"] = count_images
+    data["count_videos"] = count_videos
+
+    with open("data/data.json", "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
 
 def main():
     get_data()
